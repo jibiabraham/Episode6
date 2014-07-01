64c4440d39429255048f1b35d2b5dfc8ae27cfa1
Jibi Abraham <jibi.abraham@pagalguy.com>
2014-07-01 13:04:47 +0530

    Some minor modifications to make it slighly more palatable

--- app/src/main/java/com/studio/nn/episode6/CompositeVerticalPagerFragment.java
+++ app/src/main/java/com/studio/nn/episode6/CompositeVerticalPagerFragment.java
@@ -55,10 +55,6 @@ public class CompositeVerticalPagerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.composite_vertical_page, container, false);
-
-        TextView discussionTitle = (TextView) view.findViewById(R.id.discussion);
-        discussionTitle.setText("Discussion " + Integer.toString(sectionNumber));
-
         mHorizontalPager = (ViewPager) view.findViewById(R.id.fragment_composite_pager);
 
         populateHorizontalPager();
--- app/src/main/java/com/studio/nn/episode6/VerticalPageFragment.java
+++ app/src/main/java/com/studio/nn/episode6/VerticalPageFragment.java
@@ -37,8 +37,6 @@ public class VerticalPageFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.vertical_page_instance, container, false);
-        TextView postTitle = (TextView) view.findViewById(R.id.post);
-        postTitle.setText("Post " + Integer.toString(position));
         return view;
     }
 }
